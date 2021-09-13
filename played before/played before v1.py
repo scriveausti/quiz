2141c562b18bb670358b6171f487baf204b507fa
scriveausti <scrive.austi@awatapu.school.nz>
2021-09-13 13:03:23 +1200

    played before v1

--- played before/played before v1.py
+++ played before/played before v1.py
@@ -1,9 +1,12 @@
 while True:
     played_before = input("have you played before?").strip().lower()
-    if played_before == "yes":
+    if played_before == "yes" or played_before == "y":
+        print("starting quiz")
         break
-    elif played_before == "no":
+    elif played_before == "no" or played_before == "n":
         print("instructions")
+        input("press enter to continue")
+        print("game starting")
         break
     else:
         print("<error> please enter yes or no")
