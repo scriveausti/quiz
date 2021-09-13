959d5f15542432a568a6805aa203f19b51c34136
scriveausti <scrive.austi@awatapu.school.nz>
2021-09-13 12:56:58 +1200

    played before v1

---
+++ played before/played before v1.py
@@ -0,0 +1,9 @@
+while True:
+    played_before = input("have you played before?")
+    if played_before == "yes":
+        break
+    elif played_before == "no":
+        print("instructions")
+        break
+    else:
+        print("<error> please enter yes or no")
