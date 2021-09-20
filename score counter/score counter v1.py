ef496af038f6ff92b392105bc677658767cdd7ae
scriveausti <scrive.austi@awatapu.school.nz>
2021-09-20 12:12:52 +1200

    score counter v1

--- score counter/score counter v1.py
+++ score counter/score counter v1.py
@@ -1,11 +1,11 @@
 score = 0
-win = 0
-if win == True:
+win = False
+if win == False:
     print("")
     print("Your score has been decreased by 1")
     score -= 1
     print("your total score is now {}".format(score))
-elif win == False:
+elif win == True:
     print("")
     print("your score has increased by 3")
     score += 3
