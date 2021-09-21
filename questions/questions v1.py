b0c8f2d665c54150ca321758714afc41cd5fee14
scriveausti <scrive.austi@awatapu.school.nz>
2021-09-21 13:28:27 +1200

    questions v1

---
+++ questions/questions v1.py
@@ -0,0 +1,32 @@
+r =1
+
+import random
+
+question = [        "what is the best colour?",   "what is the best colour?"]
+answer1 = [         "blue",                       "blue"]
+answer2 = [         "orange",                     "orange"]
+answer3 = [         "red",                        "red"]
+answer4 = [         "green",                      "green"]
+correct_answer = [  "blue",                       "orange"]
+
+
+question_num = random.randint(0,1)
+while True:
+    print("")
+    print("question {} {}".format(r, question[question_num]))
+    print("a. {}" .format(answer1[question_num]))
+    print("b. {}" .format(answer2[question_num]))
+    print("c. {}" .format(answer3[question_num]))
+    print("d. {}" .format(answer4[question_num]))
+    answer = input("A, B, C or D").strip().lower()
+    if answer == "a" :
+        answer = answer1[question_num]
+    elif answer == "b" :
+        answer = answer2[question_num]
+    elif answer == "c" :
+        answer = answer3[question_num]
+    elif answer == "d" :
+        answer = answer4[question_num]
+    else:
+        print("")
+        print("<error> please enter A, B, C or D")
