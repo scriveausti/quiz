d0149a79ae90631a7f1dcae4842758cf7c7e96bf
scriveausti <scrive.austi@awatapu.school.nz>
2021-09-27 12:44:39 +1300

    questions v1

--- questions/questions v1.py
+++ questions/questions v1.py
@@ -21,12 +21,21 @@ while True:
     answer = input("A, B, C or D").strip().lower()
     if answer == "a" :
         answer = answer1[question_num]
+        break
     elif answer == "b" :
         answer = answer2[question_num]
+        break
     elif answer == "c" :
         answer = answer3[question_num]
+        break
     elif answer == "d" :
         answer = answer4[question_num]
+        break
     else:
         print("")
         print("<error> please enter A, B, C or D")
+
+if answer == correct_answer:
+    win = True
+else:
+    win = False
